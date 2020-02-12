80da31befc615f67ee3344c52c8cb8e5a30fa917
Juan Carlos Tique Rangel <47435355+JuanCarlos-TiqueRangel@users.noreply.github.com>
2020-02-12 18:48:15 -0500

    Update Pos.py

--- Rover_ROS/Pos.py
+++ Rover_ROS/Pos.py
@@ -2,58 +2,100 @@
 import serial
 import rospy
 import numpy as np
-from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 from nav_msgs.msg import Odometry
 
 class ubication(object):
         def __init__(self):
                 self.pub = rospy.Publisher('/Position', Odometry, queue_size=10)
 
-                self.enc_left = 0
-                self.enc_right = 0
-                self.enc_left_1 = 0
-                self.enc_right_1 = 0
-                self.enc_ts = 1.0/20.0
-                self.Rpm_left = 0.0
-                self.Rpm_right =0.0
+		self.enc_ts = 0.05
 
-                rospy.Subscriber('/enc_data', Odometry, self.read_enc)
-                #rospy.Subscriber("informacion de la imu")
-                #rospy.Subscriber("informacion del magnetometro")
-                #rospy.Subscriber("informacion del gps")
+		self.W_Left = 0.0
+		self.W_Right =0.0
+		self.W_Left_1 = 0.0
+		self.W_Right_1 = 0.0
+
+		self.WL = 0.0
+		self.WR = 0.0
+		self.WL_1 = 0.0
+		self.WR_1 = 0.0
+
+		self.Left = 0.0
+		self.Left_1 = 0.0
+		self.Right = 0.0
+		self.Right_1 = 0.0
+
+		self.PL_1 = 0.0
+		self.PR_1 = 0.0
+
+		self.counter = 0.0
+		self.counter_1 = 0.0
+		self.deltatiempo = 0.0
+		self.deltatiempo_1 = 0.0
+
+		rospy.Subscriber('/enc_data', JointState, self.read_enc)
+		#self.rate = rospy.Rate(10)
                 rospy.spin()
 
         def read_enc(self,data):
-                odom = Odometry()
-                self.enc_left = data.pose.covariance[5]
-                enc_disL = self.enc_left - self.enc_left_1
-                self.enc_left_1 = self.enc_left
+		odom = Odometry()
+		self.counter = data.velocity[2]
+		self.deltatiempo = self.counter - self.counter_1
 
-                self.enc_right = data.pose.covariance[6]
-                enc_disR = self.enc_right - self.enc_right_1
-                self.enc_right_1 = self.enc_right
+		if self.deltatiempo == 0:
+			self.deltatiempo = self.deltatiempo_1
+
+		# Angular Velocity rad/s and Lineal Velocity m/s
+		# 1500 son pulsos por RPM que tiene el encoder
+
+		self.W_Left = (data.velocity[0]-self.PL_1)*(2*np.pi)/(1500*self.enc_ts*self.deltatiempo)
+		self.W_Right = (data.velocity[1]-self.PR_1)*(2*np.pi)/(1500*self.enc_ts*self.deltatiempo)
+
+		#saturar la aceleracion
+		if abs(self.W_Left - self.W_Left_1) > 20.0 or abs(self.W_Right - self.W_Right_1) > 20.0:
+			self.W_Left = self.W_Left_1
+			self.W_Right = self.W_Right_1
+
+		#filtro buttter
+		self.Left = self.W_Left
+		self.Right = self.W_Right
+		self.WL = 0.2452*self.Left + 0.2452*self.Left_1 + 0.5095*self.WL
+		self.WR = 0.2452*self.Right + 0.2452*self.Right_1 + 0.5095*self.WR
+
+		#publish data
+		odom.twist.twist.angular.x = self.WL # Velocidad angular motor izquierdo
+		odom.twist.twist.angular.y = self.WR # Velocidad angular motor derecho
+
+		#odom.twist.twist.linear.x = vel_left # velocidad sin filtro
+		#odom.twist.twist.linear.y = vel_right # velocidad sin filtro
+
+		odom.header.stamp = rospy.get_rostime()
+		self.pub.publish(odom)
 
 
-                # Angular Velocity and Lineal Velocity
-                # 1500 son pulsos por RPM que tiene el encoder
-                self.Rpm_left = (2*np.pi*enc_disL)/(1500.0*self.enc_ts)
-                vel_left = self.Rpm_left*0.2
+		#update variables
+		self.counter_1 = self.counter
+		self.deltatiempo_1 = self.deltatiempo
 
-                self.Rpm_right = (2*np.pi*enc_disR)/(1500.0*self.enc_ts)
-                vel_right = self.Rpm_right*0.2
+		self.PL_1 =  data.velocity[0]
+		self.PR_1 = data.velocity[1]
 
-                odom.twist.twist.angular.x = self.Rpm_left # Velocidad angular motor izquierdo
-                odom.twist.twist.linear.x = vel_left # velocidad lineal motor izquierdo
-                odom.twist.twist.angular.y = self.Rpm_right # Velocidad angular motor derecho
-                odom.twist.twist.linear.y = vel_right # velocidad lineal motor derecho
+		self.Left_1 = self.Left
+		self.Right_1 = self.Right
 
-                odom.header.stamp = rospy.get_rostime()
-                self.pub.publish(odom)
+		self.W_Left_1 = self.W_Left
+		self.W_Right_1 = self.W_Right
+
+		#print str(self.W_left) +"\t"+ str(self.W_left*9.5493)
+		#print "\t"
+		#print str(self.W_Right) + "\t" + str(self.W_Right_1) + '\t' + str(data.pose.covariance[6])
+		#print self.WL
 
 if __name__=='__main__':
         try:
                 rospy.init_node('Position',anonymous=True, disable_signals=True)
-                print "Nodo Creado"
+		print "Nodo POS Creado"
                 cv = ubication()
         except rospy.ROSInterruptException:
                 pass
